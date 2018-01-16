a0288f34490081746b4bd13c4e68df87f51c997d
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-01-16 13:25:34 +0100

    Create module currency_monthly_rate

---
+++ currency_monthly_rate/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Currency Monthly Rate",
+    "version": "11.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Accounting",
+    "description": """Use monthly currency rate average""",
+    "website": "http://www.camptocamp.com",
+    "depends": ['base'],
+    "data": [
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+        'views/res_currency.xml'
+    ],
+    'installable': True,
+}
